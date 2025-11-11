a85d101729c200ba7f325937cc9da8b20560170a
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-11 13:25:12 +0100

    first commit

---
+++ javascripit/script.js
@@ -0,0 +1,47 @@
+/* Dato un array di oggetti rappresentante un team di un’azienda,
+ creare una pagina dedicata  in cui 
+mostrare una card per ciascun componente. */
+
+/* Curare meglio l'aspetto grafico
+Rendere l’esercizio responsive, mandando a capo le card
+Aggiungere un form di agginta membri che permetta di visualizzare il nuovo membro sulla
+pagina (usate una foto qualunque, anche vostra se volete sentirvi parte del team! 😀)*/
+
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png",
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png",
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png",
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png",
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png",
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png",
+  },
+];
